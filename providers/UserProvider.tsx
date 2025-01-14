6eb2ab813c6fd49c4cba325bddf249c55b575b27
Raymond Goo <raymond.goo124@gmail.com>
2025-01-14 17:01:15 +0800

    feat: load user state from json.

--- providers/UserProvider.tsx
+++ providers/UserProvider.tsx
@@ -7,13 +7,22 @@ interface User {
   name: string
   email: string
   description: string
+  token: string
 }
 
 export const UserContext = createContext<User | null>(null);
 
 export const UserProvider = (props: {children: ReactNode}) => {
+  if (typeof localStorage === "undefined") return;
+  const userData = localStorage.getItem("user")
+  let user: User | null;
+  if (!userData) {
+    user = null;
+  } else {
+    user = JSON.parse(userData)
+  }
   return (
-    <UserContext value={null}>
+    <UserContext value={user}>
       {props.children}
     </UserContext>
   )
