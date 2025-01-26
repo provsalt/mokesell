22dd8eef7cc3607f923bbee761b9e53543e6db9a
Raymond Goo <raymond.goo124@gmail.com>
2025-01-26 23:05:41 +0800

    feat: Mobile responsive navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -19,9 +19,12 @@ export const Navbar = () => {
       localStorage.removeItem("user");
     }
   };
-  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [width, setWidth] = useState<number>(
+    typeof window === "undefined" ? 0 : window.innerWidth,
+  );
 
   const handleWindowSizeChange = () => {
+    if (typeof window === "undefined") return;
     setWidth(window.innerWidth);
   };
 
@@ -40,14 +43,52 @@ export const Navbar = () => {
 
   if (user) {
     return (
-      <nav className="flex p-4 justify-between text-lg">
-        <p className="font-bold text-lg">
-          <Link href="/">Mokesell</Link>
-        </p>
-        {width >= 768 && (
+      <nav className="flex flex-col p-4">
+        <div className="flex py-4 md:py-2 items-center justify-between text-lg">
+          <p className="font-bold text-lg">
+            <Link href="/">Mokesell</Link>
+          </p>
+          {width >= 768 && (
+            <div className="flex">
+              <Input
+                className="min-w-[25vw]"
+                onInput={(e) => {
+                  const target = e.target as HTMLInputElement;
+                  setSearchQuery(target.value);
+                }}
+                value={searchQuery}
+                placeholder="Search"
+              />
+              <Button onClick={search}>
+                <Search />
+              </Button>
+            </div>
+          )}
+          <div className="flex gap-2">
+            <Link href="/">
+              <Button className="dark:bg-transparent">Sell</Button>
+            </Link>
+            <Link href="/user">
+              <Button className="dark:bg-transparent">
+                <User />
+              </Button>
+            </Link>
+            <Link href="/chats">
+              <Button className="dark:bg-transparent">
+                <MessageCircle />
+                <p className="sr-only">Chat</p>
+              </Button>
+            </Link>
+            <Button onClick={logout} className="dark:bg-transparent">
+              <LogOut />
+              <p className="sr-only">Logout</p>
+            </Button>
+          </div>
+        </div>
+        {width <= 768 && (
           <div className="flex">
             <Input
-              className=""
+              className="min-w-[25vw]"
               onInput={(e) => {
                 const target = e.target as HTMLInputElement;
                 setSearchQuery(target.value);
@@ -60,38 +101,46 @@ export const Navbar = () => {
             </Button>
           </div>
         )}
-        <div className="flex gap-2">
-          <Link href="/">
-            <Button className="dark:bg-transparent">Sell</Button>
-          </Link>
-          <Link href="/user">
-            <Button className="dark:bg-transparent">
-              <User />
-            </Button>
-          </Link>
-          <Link href="/chats">
-            <Button className="dark:bg-transparent">
-              <MessageCircle />
-              <p className="sr-only">Chat</p>
-            </Button>
-          </Link>
-          <Button onClick={logout} className="dark:bg-transparent">
-            <LogOut />
-            <p className="sr-only">Logout</p>
-          </Button>
-        </div>
       </nav>
     );
   }
   return (
-    <nav className="flex p-4 justify-between text-lg">
-      <p className="font-bold">
-        <Link href="/">Mokesell</Link>
-      </p>
-      {width >= 768 && (
+    <nav className="flex p-4 flex-col">
+      <div className="flex py-4 md:py-2 items-center justify-between text-lg">
+        <p className="font-bold">
+          <Link href="/">Mokesell</Link>
+        </p>
+        {width >= 768 && (
+          <div className="flex">
+            <Input
+              className="min-w-[25vw]"
+              onInput={(e) => {
+                const target = e.target as HTMLInputElement;
+                setSearchQuery(target.value);
+              }}
+              value={searchQuery}
+              placeholder="Search"
+            />
+            <Button onClick={search}>
+              <Search />
+            </Button>
+          </div>
+        )}
+        <div className="flex gap-4">
+          <Link href="/signup">
+            <Button className="dark:bg-transparent text-gray-50 dark:text-gray-50">
+              Sign up
+            </Button>
+          </Link>
+          <Link href="/login">
+            <Button className="bg-blue-500">Login</Button>
+          </Link>
+        </div>
+      </div>
+      {width <= 768 && (
         <div className="flex">
           <Input
-            className=""
+            className="min-w-[25vw]"
             onInput={(e) => {
               const target = e.target as HTMLInputElement;
               setSearchQuery(target.value);
@@ -104,16 +153,6 @@ export const Navbar = () => {
           </Button>
         </div>
       )}
-      <div className="flex gap-4">
-        <Link href="/signup">
-          <Button className="dark:bg-transparent text-gray-50 dark:text-gray-50">
-            Sign up
-          </Button>
-        </Link>
-        <Link href="/login">
-          <Button className="bg-blue-500">Login</Button>
-        </Link>
-      </div>
     </nav>
   );
 };
