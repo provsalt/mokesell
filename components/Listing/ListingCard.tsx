00f797469557b0862427c4b2e506e63559cb89ec
pradyunL <s10266910@connect.np.edu.sg>
2025-01-26 23:26:50 +0800

    Fetched and Displayed listings from database

--- components/Listing/ListingCard.tsx
+++ components/Listing/ListingCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
-export const ListingCard = (props: { name?: string, price?: string; condition?: string, imageSource?: string } ) => {
-  if (!props || !props.name || !props.price || !props.condition || !props.imageSource) {
+export const ListingCard = (props: { name?: string, price?: string; condition?: string, images?: Array<{url: string, position: number}> } ) => {
+  if (!props || !props.name || !props.price || !props.condition || !props.images || props.images.length === 0) {
     return (
       <div className="space-y-2">
         <div className="aspect-square animate-pulse bg-gray-200 rounded-lg"></div>
@@ -13,13 +13,19 @@ export const ListingCard = (props: { name?: string, price?: string; condition?: 
       </div>
     )
   }
+
+  const sortedImages = props.images.sort((a, b) => a.position - b.position);
   return (
-    <div className="space-y-2">
-      <Image className="aspect-square bg-gray-200 rounded-lg" src={props.imageSource} alt={props.name} />
+    <div className="space-y-2 drop-shadow-lg">
+      <img className="aspect-square bg-gray-200 rounded-lg object-cover"
+      src={sortedImages[0].url} 
+      alt={props.name} 
+      width={300} 
+      height={300}/>
      <div className="space-y-1">
        <p className="text-lg text-gray-600">{props.name}</p>
        <p className="font-medium">{props.price}</p>
-       <p className="text-gray-500">{props.condition}</p>
+       <p className="text-gray-500 capitalize">{props.condition.replace('_', ' ')}</p>
      </div>
    </div>
  )
