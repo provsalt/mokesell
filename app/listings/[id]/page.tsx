d7b337fe9c3073e860ca0ffadcb6cb1cfa770f3f
Raymond Goo <raymond.goo124@gmail.com>
2025-01-28 00:54:21 +0800

    fix: Fix build error due to unused dependencies

--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 interface Listing {
   id: number;
   title: string;
--- components/Listing/ListingCard.tsx
+++ components/Listing/ListingCard.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export const ListingCard = (props: {
   name?: string;
   price?: string;
