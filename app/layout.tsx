37df3221081449b6636161869b1128900b424333
Raymond Goo <raymond.goo124@gmail.com>
2025-02-06 15:08:58 +0800

    fix(layout): use flex-col to increase the height.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {Navbar} from "@/components/Navbar/Navbar";
-import {UserProvider} from "@/providers/UserProvider";
-import {Toaster} from "@/components/ui/toaster";
+import { Navbar } from "@/components/Navbar/Navbar";
+import { UserProvider } from "@/providers/UserProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Mokesell",
@@ -15,17 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen"
-      >
-         <UserProvider>
-           <div className="sticky top-0 z-40 dark:bg-gray-950 dark:text-gray-50 bg-gray-50 text-gray-950 drop-shadow-md">
-             <Navbar/>
-           </div>
-           <main className="flex flex-1 dark:bg-gray-950 dark:text-gray-50 bg-gray-50 text-gray-950 ">
-             {children}
-           </main>
-           <Toaster />
-         </UserProvider>
+      <body className="flex flex-col min-h-screen">
+        <UserProvider>
+          <div className="sticky top-0 z-40 dark:bg-gray-950 dark:text-gray-50 bg-gray-50 text-gray-950 drop-shadow-md">
+            <Navbar />
+          </div>
+          <main className="flex flex-1 flex-col dark:bg-gray-950 dark:text-gray-50 bg-gray-50 text-gray-950 ">
+            {children}
+          </main>
+          <Toaster />
+        </UserProvider>
       </body>
     </html>
   );
