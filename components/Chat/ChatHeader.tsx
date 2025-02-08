0c86e2818ade81a701d6f1d8dff17e2251ec2be4
pradyunL <s10266910@connect.np.edu.sg>
2025-02-08 23:35:46 +0800

    feat: chat displays messages from database

---
+++ components/Chat/ChatHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+
+interface ChatHeaderProps {
+  listingTitle: string;
+  listingImage?: string | null;
+}
+
+export const ChatHeader: React.FC<ChatHeaderProps> = ({
+  listingTitle,
+  listingImage,
+}) => {
+  return (
+    <div className="p-4 border-b bg-white z-30">
+      <div className="flex items-center gap-3">
+        {listingImage ? (
+          <Image
+            src={listingImage}
+            alt="Listing"
+            className="w-10 h-10 rounded-full object-cover"
+          />
+        ) : (
+          <div className="w-10 h-10 bg-gray-200 rounded-full flex-shrink-0" />
+        )}
+        <div className="font-medium truncate">{listingTitle}</div>
+      </div>
+    </div>
+  );
+};
